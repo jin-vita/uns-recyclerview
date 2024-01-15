569182c842f66d19c3274948052f673ec2bf9ed1
VITA-DELL <jhj9144@gmail.com>
2024-01-15 15:29:02 +0900

    feat apply viewBinding

--- app/src/main/java/org/techtown/uns_recyclerview/MainActivity.kt
+++ app/src/main/java/org/techtown/uns_recyclerview/MainActivity.kt
@@ -2,10 +2,12 @@ package org.techtown.uns_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.techtown.uns_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
